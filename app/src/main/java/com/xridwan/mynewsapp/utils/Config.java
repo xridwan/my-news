32b8baa4cba6be8d0e13da5b1925fa2b14c93b09
Ridwan <80747655+xridwan@users.noreply.github.com>
2025-06-07 23:26:05 +0700

    Update Config.java

--- app/src/main/java/com/xridwan/mynewsapp/utils/Config.java
+++ app/src/main/java/com/xridwan/mynewsapp/utils/Config.java
@@ -4,7 +4,7 @@ import com.xridwan.mynewsapp.network.ApiClient;
 import com.xridwan.mynewsapp.network.ApiInterface;
 
 public class Config {
-    public static final String API_KEY = "c2177d86f19a48eba585fe2e67612478";
+    public static final String API_KEY = "";
     public static final String BASE_URL = "https://newsapi.org/v2/";
 
     public static ApiInterface apiInterface(){
